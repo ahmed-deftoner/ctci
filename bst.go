35004fe9952288d03e93f20e7c4f119f093fd8a6
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-10-14 15:56:39 +0500

    inorderel

--- bst.go
+++ bst.go
@@ -24,6 +24,28 @@ func Insert(root *Node, key int) {
 		Insert(root.left, key)
 	}
 }
-Delete(root *Node, key int)
+
+func InorderElement(cur *Node) *Node {
+	for curr.left != nil {
+		cur = cur.left
+	}
+	return &cur;
+}
+
+func Delete(root *Node, key int) {
+	if root.item == key {
+		root = nil
+	} else {
+		if root.left == nil {
+			root = root.right
+			root.right = nil
+		} else if root.right == nil {
+			root = root.left
+			root.left = nil
+		} else {
+
+		}
+	}
+}
 Search(root *Node, key int)
 
