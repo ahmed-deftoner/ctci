c0e283412bb7ec54b30c27fe4ce0b47fede33044
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2023-02-21 00:08:09 +0500

    add at start

--- linkedList.ts
+++ linkedList.ts
@@ -8,74 +8,79 @@ class LinkedList<T> {
     private tail: Node<T>;
 
     constructor() {
-        this.head = undefined
-        this.tail = undefined
+        this.head = undefined;
+        this.tail = undefined;
     }
 
-    addNode(data: T) {
+    addAtStart(data: T) {
+        const newNode: Node<T> = {
+            data: data, 
+            next: this.head
+       };
+       this.head = newNode;
+    }
+
+    addAtEnd(data: T) {
         const newNode: Node<T> = {
              data: data, 
              next: undefined
-        }
+        };
         if (this.head == undefined) {
-            this.head = newNode
+            this.head = newNode;
         } else {
-            let temp: Node<T> = this.head
+            let temp: Node<T> = this.head;
             while (temp.next != undefined) {
-                temp = temp.next
+                temp = temp.next;
             }
-            temp.next = newNode
+            temp.next = newNode;
         }
-        this.tail = newNode
+        this.tail = newNode;
     }
 
     deleteNode(data: T) {
         if (this.head.data == data) {
-            this.head = undefined  
+            this.head = undefined;
         } else {
-            let prev: Node<T>
-            let current: Node<T> = this.head
+            let prev: Node<T>;
+            let current: Node<T> = this.head;
             while (current.next != undefined) {
-                prev = current
-                current = current.next
+                prev = current;
+                current = current.next;
                 if (current.data == data) {
-                    prev.next = current.next
+                    prev.next = current.next;
                 }
             }
         }
     }
 
     print() {
-        let temp: Node<T> = this.head
+        let temp: Node<T> = this.head;
         while (temp != undefined) {
-            console.log(`${temp.data} ->`)
-            temp = temp.next
+            console.log(`${temp.data} ->`);
+            temp = temp.next;
         }
     }
 
     peekFirst(): T | undefined {
         if (this.head == undefined) {
-            return undefined
+            return undefined;
         }
-        return this.head.data
+        return this.head.data;
     }
 
     peekLast(): T | undefined {
         if (this.head == undefined) {
-            return undefined
+            return undefined;
         }
-        return this.tail.data
+        return this.tail.data;
     }
 }
 
 let x: LinkedList<number> = new LinkedList<number>()
-x.addNode(23)
-x.addNode(44)
-x.addNode(65)
-x.print()
-x.deleteNode(44)
-x.deleteNode(65)
-x.deleteNode(23)
+x.addAtEnd(23)
+x.addAtEnd(44)
+x.addAtEnd(65)
+x.addAtStart(56)
 x.print()
 console.log(x.peekFirst())
 console.log(x.peekLast())
