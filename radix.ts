f24d87168ef82e444701181b1c585297a986a1d1
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2023-02-27 20:29:06 +0500

    done

--- radix.ts
+++ radix.ts
@@ -3,7 +3,7 @@
  * @description radix sort works by ordering each digit based on it's position relative to an exponent e.g. base 10. It uses a stable sorting algorithm to sort digits (counting sort in this example).
  * @see [Radix Sort](https://www.javatpoint.com/radix-sort)
  * @example radixSort([8, 3, 5, 1, 4, 2]) = [1, 2, 3, 4, 5, 8]
- * @Complexity_Analysis
+ * @complexity_Analysis
  * Space complexity - O(n + k)
  * Time complexity 
  *      Best case   -   O(n + k)
@@ -14,7 +14,6 @@ function radixSort(arr: Array<number>): Array<number> {
   // get max from array
   let max: number = Math.max(...arr);
 
-  console.log(arr);
   // Apply counting sort to sort elements based on place value.
   for (let exp: number = 1; max / exp > 0; exp *= 10){
       arr = countingSort(arr, exp);
@@ -43,8 +42,9 @@ function countingSort(arr: Array<number>, exponent: number): Array<number> {
   return output;
 }
 
-
-const arr = [4, 3, 2, 1];
-console.log(arr);
-const res = radixSort(arr);
+let res = radixSort([1, 4, 2, 5, 9, 6, 3, 8, 10, 7]);
+console.log(res);
+res = radixSort([10, 9, 800, 7, 6, 5, 40, 3, 2, 1]);
+console.log(res);
+res = radixSort([1, 4, 2, 9, 5000, 7, 3, 8, 10, 6]);
 console.log(res);
