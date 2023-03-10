d94d044e7ac01ae21ee638bde494f48049c6ee98
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2023-03-10 20:49:36 +0500

    bst

---
+++ bst.ts
@@ -0,0 +1,59 @@
+interface Node {
+    data: number,
+    left: Node | null,
+    right: Node | null
+}
+
+class BST {
+    private root: Node | null;
+
+    constructor() {
+        this.root = null;
+    }
+
+    newNode(val: number): Node {
+        return {
+            data: val,
+            left: null,
+            right: null
+        } as Node;
+    }
+
+    insertNode(val: number, root: Node | null): Node {
+        if (root == null) {
+            return this.newNode(val);        
+        }
+        if (val > root?.data) {
+            root.right = this.insertNode(val, root.right);
+        } else {
+            root.left = this.insertNode(val, root.left);
+        } 
+        return root;
+    }
+
+    inorder(root: Node | null) {
+        if (root == null) {
+            return;
+        }
+        this.inorder(root?.left);
+        console.log(root.data);
+        this.inorder(root.right);
+    }
+}
+
+let node: Node;
+const bst: BST = new BST();
+node = bst.insertNode(23, null);
+node = bst.insertNode(43, node);
+node = bst.insertNode(2, node);
+node = bst.insertNode(98, node);
+node = bst.insertNode(67, node);
+node = bst.insertNode(4, node);
+console.log(node);
+bst.inorder(node);
+
+
+
+
+
+
